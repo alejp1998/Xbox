4e4b417455c16ab2fd228331ed799ab6f4f3049c
Alejandro Jarabo Peñas <a.jarabo@alumnos.upm.es>
2019-05-02 16:33:50 +0200

    Escritura en fichero unico

--- sample.py
+++ sample.py
@@ -19,64 +19,45 @@ def showIf(boolean, ifTrue, ifFalse=" "):
 
 # Instantiate the controller
 joy = xbox.Joystick()
-nwait = 0
+
 # Show various axis and button states until Back button is pressed
 print("Xbox controller printing in piTankEx")
 while not joy.Back():
 
     #Control de los botones pulsados
-    f1 = open("../piTankEx/xbox360-1.txt","w+")
-
-    if nwait < 19 and nwait != 0 :
-        nwait += 1
-    elif joy.rightX() > 0.5:
-        f1.write("R")
-        nwait = 1
+    if joy.rightX() > 0.5:
+        str1 = "R"
     elif joy.rightX() < -0.5:
-        f1.write("L")
-        nwait = 1
+        str1 = "L"
     elif joy.rightY() > 0.5:
-        f1.write("U")
-        nwait = 1
+        str1 = "U"
     elif joy.rightY() < -0.5:
-        f1.write("D")
-        nwait = 1
+        str1 = "D"
     elif joy.A(): 
-        f1.write("A")
-        nwait = 1
+        str1 = "A"
     elif joy.B(): 
-        f1.write("B")
-        nwait = 1
+        str1 = "B"
     elif joy.Y(): 
-        f1.write("Y")
-        nwait = 1
+        str1 = "Y"
     elif joy.X(): 
-        f1.write("X")
-        nwait = 1
+        str1 = "X"
     else: 
-        f1.write("N")
-        nwait = 0    
+        str1 = "N"   
 
     f1.close()
 
     #Control de joystick izquierdo para regulacion de velocidad de ruedas
-    f2 = open("../piTankEx/xbox360-2.txt","w+")
-
-    if joy.leftX() > 0.2 or joy.leftX() < -0.2: 
-        f2.write("%1.3f" % joy.leftX())
+    if joy.leftX() > 0.4 or joy.leftX() < -0.4: 
+        str2 = "%1.3f" % joy.leftX()
     else: 
-        f2.write("0.0")
-    
-    f2.close()
+        str2 = "0.0"
 
-    f3 = open("../piTankEx/xbox360-3.txt","w+")
-
-    if joy.leftY() > 0.2 or joy.leftY() < -0.2:
-        f3.write("%1.3f" % joy.leftY())
+    if joy.leftY() > 0.4 or joy.leftY() < -0.4:
+        str3 = "%1.3f" % joy.leftY()
     else:
-        f3.write("0.0")
-    
-    f3.close()
+        str3 = "0.0"
 
+    f1 = open("../piTankEx/xbox360.txt","w+")
+    f1.write(str1 + " " + str2 + " " + str3)
 # Close out when done
 joy.close()
